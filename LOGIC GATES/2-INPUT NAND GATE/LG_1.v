1c4121fa1ec2046ba7df36abc673a826225ff6b0
sayeekumar332 <64132325+sayeekumar332@users.noreply.github.com>
2021-02-13 14:49:09 +0530

    Update LG_1.v

--- LOGIC GATES/2-INPUT NAND GATE/LG_1.v
+++ LOGIC GATES/2-INPUT NAND GATE/LG_1.v
@@ -1,1 +1,8 @@
+// Two Input NAND gate
+module LG_1(input a , input b , output y); // Positive Logic Version of NAND gate
+assign y = ~(a & b);
+endmodule
 
+module LG_1(input a , input b , output y);// Negative Logic Version of NAND gate
+assign y = ~a | ~b;
+endmodule
