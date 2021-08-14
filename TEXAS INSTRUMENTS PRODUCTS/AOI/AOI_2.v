ca74fd1f35f631f1b259cab436ccc22cd2c994b2
SAYEEKUMAR <64132325+sayeekumar332@users.noreply.github.com>
2021-08-14 15:54:37 +0530

    Create AOI_2.v

---
+++ TEXAS INSTRUMENTS PRODUCTS/AOI/AOI_2.v
@@ -0,0 +1,11 @@
+module AOI_2_1(input A,input B,input C,input D,input E,input F,input G,input H,output Y);
+wire a_w;
+wire b_w;
+wire c_w;
+wire d_w;
+assign a_w = A & B;
+assign b_w = C & D;
+assign c_w = E & F;
+assign d_w = G & H;
+assign Y = ~(a_w | b_w | c_w | d_w);
+endmodule
