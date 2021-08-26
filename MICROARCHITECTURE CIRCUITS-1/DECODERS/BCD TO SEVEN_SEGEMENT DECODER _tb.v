ec98d22c95800ed97e005c0fba76cf17a6bd598c
SAYEEKUMAR <64132325+sayeekumar332@users.noreply.github.com>
2021-08-26 10:01:06 +0530

    Update BCD TO SEVEN_SEGEMENT DECODER _tb.v

--- MICROARCHITECTURE CIRCUITS-1/DECODERS/BCD TO SEVEN_SEGEMENT DECODER _tb.v
+++ MICROARCHITECTURE CIRCUITS-1/DECODERS/BCD TO SEVEN_SEGEMENT DECODER _tb.v
@@ -1,60 +1,61 @@
-module FOURTO10_tb;
+module BCDTOSEVEN_1_tb;
 
 	// Inputs
-	reg A;
-	reg B;
-	reg C;
+	reg LT;
+	reg BI;
+	reg RBI;
 	reg D;
+	reg C;
+	reg B;
+	reg A;
 
 	// Outputs
-	wire Y0;
-	wire Y1;
-	wire Y2;
-	wire Y3;
-	wire Y4;
-	wire Y5;
-	wire Y6;
-	wire Y7;
-	wire Y8;
-	wire Y9;
+	wire a;
+	wire b;
+	wire c;
+	wire d;
+	wire e;
+	wire f;
+	wire g;
+	
 
 	// Instantiate the Unit Under Test (UUT)
-	FOURTO10 uut (
-		.A(A), 
-		.B(B), 
-		.C(C), 
+	BCDTOSEVEN_1 uut (
+		.LT(LT), 
+		.BI(BI), 
+		.RBI(RBI), 
 		.D(D), 
-		.Y0(Y0), 
-		.Y1(Y1), 
-		.Y2(Y2), 
-		.Y3(Y3), 
-		.Y4(Y4), 
-		.Y5(Y5), 
-		.Y6(Y6), 
-		.Y7(Y7), 
-		.Y8(Y8), 
-		.Y9(Y9)
+		.C(C),
+		.B(B)
+		.A(A)
+		.a(a), 
+		.b(b), 
+		.c(c), 
+		.d(d), 
+		.e(e), 
+		.f(f), 
+		.g(g)
 	);
 
 	initial begin
 		// Initialize Inputs
-		A = 0;
-		B = 0;
-		C = 0;
+		LT = 1;
+		BI = 1'bx;
+		RBI = 0
 		D = 0;
+		C = 0;
+		B = 0;
+		A = 0;
 
 		// Wait 100 ns for global reset to finish
 		#100;
-      A = 0;
-		B = 1;
+		LT = 1;
+		BI = 1'bx;
+		RBI = 1'bx;
+                D = 0;
 		C = 0;
-		D = 1;  
-		
-      #100;
-      A = 0;
 		B = 0;
-		C = 0;
-		D = 1; 
-	end
-      
+		A = 1;  
+		
+	end      
 endmodule
