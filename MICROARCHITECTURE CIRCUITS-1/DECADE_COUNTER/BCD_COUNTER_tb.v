3cb7f40e60dc20fe7f34d3375db5f6c404b86179
SAYEEKUMAR <64132325+sayeekumar332@users.noreply.github.com>
2021-09-12 14:50:57 +0530

    Create BCD_COUNTER_tb.v

---
+++ MICROARCHITECTURE CIRCUITS-1/DECADE_COUNTER/BCD_COUNTER_tb.v
@@ -0,0 +1,102 @@
+module DEC_COUNT_1_tb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire QA;
+	wire QB;
+	wire QC;
+	wire QD;
+
+	// Instantiate the Unit Under Test (UUT)
+	DEC_COUNT_1 uut (
+		.clk(clk), 
+		.rst(rst), 
+		.QA(QA), 
+		.QB(QB), 
+		.QC(QC), 
+		.QD(QD)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+
+		// Wait 100 ns for global reset to finish
+		#50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1; 
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1;
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1;
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1;
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1;
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1; 
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1;
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1;
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1;
+
+      #50;
+      clk = 1;
+		rst = 1;
+      #50;
+      clk = 0;
+		rst = 1;  		
+		// Add stimulus here
+
+	end
+      
+endmodule
