c326e6525e316c115ce32bc524051ba0af1adb9d
SAYEEKUMAR <64132325+sayeekumar332@users.noreply.github.com>
2021-09-27 13:49:48 +0530

    Create Hello_World.java

---
+++ JAVA CIRCUITS/Hello_World.java
@@ -0,0 +1,10 @@
+// In this Java Program , the string Hello World is given as command line argument in command prompt
+class Hello_World
+{
+  public static void main(String args[])
+  {
+    System.out.print(args[0] + " " + args[1]);
+  }
+}
+// In command prompt , the way of executing Java Program is to give the following text
+// java class_name args[0] args[1]........... and so on
