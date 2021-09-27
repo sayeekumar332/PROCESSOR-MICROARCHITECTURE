e70520d9161256029c5b101137bcd4147817895b
SAYEEKUMAR <64132325+sayeekumar332@users.noreply.github.com>
2021-09-27 14:02:35 +0530

    Update Hello_World.java

--- JAVA CIRCUITS/Hello_World.java
+++ JAVA CIRCUITS/Hello_World.java
@@ -3,8 +3,21 @@ class Hello_World
 {
   public static void main(String args[])
   {
-    System.out.print(args[0] + " " + args[1]);
+    System.out.print(args[0] + " " + args[1]);// + serves as String concatenation operator ; The empty double quotes serves as "space" between the two command line arguments
   }
 }
+// In command prompt , the way of compiling[i.e. creating .class files[or Bytecode] is to give the following text
+// javac class_name.java
 // In command prompt , the way of executing Java Program is to give the following text
 // java class_name args[0] args[1]........... and so on
+// The command line arguments "args[n]" , where "n" represents an integer , when separated by white spaces will be considered as separate arguments
+// The first command line argument is indexed at 0 , the second at 1 and so on.
+
+// Another way of writing the same program
+class Hello_World
+{
+  public static void main(String[] args) // The empty square braces can be placed even before the keyword "args" ; String[] args is a array of the data-type String.
+  {
+    System.out.print(args[0] + " " + args[1]); // + serves as String concatenation operator ; The empty double quotes serves as "space" between the two command line arguments
+  }
+}
