828cb222b0f23ee355d2460906c43f79de0853a5
SAYEEKUMAR <64132325+sayeekumar332@users.noreply.github.com>
2021-09-29 10:51:06 +0530

    Update Determine_User_Thread_priority.java

--- JAVA CIRCUITS/MULTI-THREADING/Determine_User_Thread_priority.java
+++ JAVA CIRCUITS/MULTI-THREADING/Determine_User_Thread_priority.java
@@ -5,8 +5,8 @@ class Multi extends Thread
 	{
 		Thread t1 = new Thread("First Thread"); // t1 and t2 are child Threads. A child Thread will have the same priority as the parent thread. When no priorities are assigned to a thread , the default value of 5 will be assigned to all the threads.
 		Thread t2 = new Thread("Second Thread");
-		//t1.setPriority(2);// Default priority of a thread in Java
-		//t2.setPriority(4);// Max. priority of a thread in Java
+		//t1.setPriority(5);// Default priority of a thread in Java
+		//t2.setPriority(10);// Max. priority of a thread in Java
 		t1.start();
 		t2.start();
 		System.out.println("The currently executing thread is " + Thread.currentThread().getName());
