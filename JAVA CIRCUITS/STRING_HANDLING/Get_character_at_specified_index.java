75059d03d9ba9bb72f933704490485a4221bab23
SAYEEKUMAR <64132325+sayeekumar332@users.noreply.github.com>
2021-09-30 18:20:41 +0530

    Update Get_character_at_specified_index.java

--- JAVA CIRCUITS/STRING_HANDLING/Get_character_at_specified_index.java
+++ JAVA CIRCUITS/STRING_HANDLING/Get_character_at_specified_index.java
@@ -29,3 +29,18 @@ char ch = s1.charAt(8);
 System.out.println(ch);//This generates String Out of Bounds Exception , because the number of indices in the string "Welcome" ranges from 0 to 7 , since the string "Welcome" is a 7 characters string
 }
 }
+
+Code No.4 : Access the first character and last character of a string stored in the String object
+public class String_1
+{
+public static void main(String args[])
+{
+String s1 = "Welcome";
+int strlength = s1.length();// length() method is used to determine the number of indices in a given string object
+char ch = s1.charAt(0);// This displays the first character of string
+System.out.println(ch);
+char ch_1 = s1.charAt(strlength-1); //The last character is present at the index = Number of characters in the string - 1
+System.out.println(ch_1);// This prints the last character of the string
+}
+}
+ 
